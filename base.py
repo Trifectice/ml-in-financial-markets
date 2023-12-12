40dbf641233ed6eeae76519b562e6186cd752ddb
Scott Keller <smkdbv911@gmail.com>
2023-12-12 16:19:25 -0700

    adding training and trading dates, and assets name for portfilios data set

--- base.py
+++ base.py
@@ -18,3 +18,22 @@ from finrl.meta.preprocessor.preprocessors import FeatureEngineer, data_split
 from finrl.config import INDICATORS
 
 import itertools
+
+TRAIN_START_DATE = '2009-01-01'
+TRAIN_END_DATE = '2020-07-01'
+TRADE_START_DATE = '2020-07-01'
+TRADE_END_DATE = '2023-05-01'
+
+symbols = [
+    'aapl',
+    'msft',
+    'meta',
+    'ibm',
+    'hd',
+    'cat',
+    'amzn',
+    'intc',
+    't',
+    'v',
+    'gs'
+]
